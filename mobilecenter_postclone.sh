193ba4c6de19a783ebaed295b1a896873ff10d73
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-06-27 12:44:32 +0300

    Update mobilecenter_postclone.sh

--- mobilecenter_postclone.sh
+++ mobilecenter_postclone.sh
@@ -2,3 +2,6 @@
 echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
+
+echo "$AGENT_HOMEDIRECTORY/scripts/emit-tags.sh"
+head -100 $AGENT_HOMEDIRECTORY/scripts/emit-tags.sh
